51c05cd1cd0a6dd7d9e59b2fbc7a4730b223820d
Наталья <nyurjeva@mail.ru>
2023-06-17 16:52:30 +0300

    Second commit: create page /news/ for NewsPaper

--- NewsPaper/news/admin.py
+++ NewsPaper/news/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Post, PostCategory, Comment, Category, Author
 
-# Register your models here.
+
+admin.site.register(Post)
+admin.site.register(PostCategory)
+admin.site.register(Comment)
+admin.site.register(Category)
+admin.site.register(Author)
+
+
--- NewsPaper/news/views.py
+++ NewsPaper/news/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
-# Create your views here.
+
+class PostsList(ListView):
+    model = Post
+    ordering = 'create_time'
+    template_name = 'news.html'
+    context_object_name = 'news'
+
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'one_news.html'
+    context_object_name = 'post'
+
+
